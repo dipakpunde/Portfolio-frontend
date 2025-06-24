9775b401ac036e8ff93bf464b89b4617d1ff798f
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 13:26:29 +0300

    url

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "https://portfolio-backend-yj3ldqrip-dipak-pundes-projects.vercel.app";
+export const baseURL = "http://localhost:3001";
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
