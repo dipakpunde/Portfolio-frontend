4c452b8e51c7a33b175a6ba770db2b09f6badbd8
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 15:21:23 +0300

    Update Repository.js

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "https://portfolio-frontend-2hi6.vercel.app/";
+export const baseURL = "https://portfolio-frontend-omega-five.vercel.app/";
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
