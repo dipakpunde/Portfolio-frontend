446434a85592684d3453bf5bc515b1f076f04a38
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-26 09:23:00 +0300

    Update Repository.js

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:10000";
+export const baseURL = "https://portfolio-backend-6rs9.onrender.com";
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
